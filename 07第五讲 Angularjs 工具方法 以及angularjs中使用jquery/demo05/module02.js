2ffeb01cda259800e93cadfcb24372eda3760216
apn-ran <ran.yin@assuredperformance.net>
2018-05-02 07:55:07 -0700

    lecture 4 - 10

---
+++ 07第五讲 Angularjs 工具方法 以及angularjs中使用jquery/demo05/module02.js
@@ -0,0 +1,13 @@
+/**
+ * Created by htzhanglong on 2015/4/12.
+ */
+
+var app2 = angular.module("myApp2", []);
+app2.controller('secondController',function($scope){
+    $scope.name='李四';
+
+});
+app2.controller('threeController',function($scope){
+    $scope.name='王五';
+
+});
